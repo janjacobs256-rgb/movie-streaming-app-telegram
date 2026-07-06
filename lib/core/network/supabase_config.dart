b3540aa64c7c351dd5614ce1c01d65507617c2e0
George Ongoro <gtechong72@gmail.com>
2026-07-06 12:01:52 +0300

    Update backend URL to Render deployment

--- lib/core/network/supabase_config.dart
+++ lib/core/network/supabase_config.dart
@@ -4,5 +4,5 @@ class SupabaseConfig {
   static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwc2JiaHZodGVsbmRoaG5jbWp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4OTg1NzEsImV4cCI6MjA5ODQ3NDU3MX0.QMdmt2TYfbDHXcpueeINLZDo8K_btS1axpk1Qa04oB0';
 
   // Replace with your local backend URL or deployed backend URL (e.g. Hugging Face Space URL)
-  static const String backendUrl = 'http://192.168.100.198:8000';
+  static const String backendUrl = 'https://movie-streaming-app-telegram.onrender.com';
 }
